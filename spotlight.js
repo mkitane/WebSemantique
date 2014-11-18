7f0f54ff12b1c1007a073aef098b8cb4188658a6
amelrhazi <elrhazi.amine@gmail.com>
2014-11-18 11:42:37 +0100

    Spotlight fin

--- spotlight.js
+++ spotlight.js
@@ -1,12 +1,31 @@
 var request = require('superagent');
 
-console.log(process.argv[2]);
-var array = JSON.parse(process.argv[2]);
-console.log(array[0]);
+//var urls = JSON.parse(process.argv[2]);
 
-request
-      .get('http://spotlight.dbpedia.org/rest/annotate?text=President%20Michelle%20Obama%20called%20Thursday%20on%20Congress%20to%20extend%20a%20tax%20break%20for%20students%20included%20in%20last%20year%27s%20economic%20stimulus%20package,%20arguing%20that%20the%20policy%20provides%20more%20generous%20assistance.&confidence=0.2&support=20')
-      .set('Accept', 'application/json')
-      .end(function(error, res){
-            //console.log(res);
-        });
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null)
+  {
+    var urls = JSON.parse(chunk);
+    
+    urls.forEach(function(url, i)
+    {
+          request
+                .get('http://spotlight.dbpedia.org/rest/annotate?confidence=0.2&support=20')
+                .query({
+                            text: url.text
+                })
+                .set('Accept', 'application/json')
+                .end(function(res){
+                      if (res.ok) {
+                            res.body.Resources.forEach(function(resource, i) {
+                                  console.log(resource['@URI']);
+                            });
+                      }
+                });      
+    });
+    
+  }
+});
