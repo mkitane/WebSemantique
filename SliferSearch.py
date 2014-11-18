b2170f60e1cb2c0aa60efda93e69c04f4daa0134
mkitane <mehdikitane@gmail.com>
2014-11-18 10:41:35 +0100

    Indentation Fix

--- SliferSearch.py
+++ SliferSearch.py
@@ -2,40 +2,41 @@
 import re
 import requests
 import json
+import sys
 
 def get_URL_SSearch(query): 
-        baseURL = "https://www.googleapis.com/customsearch/"
-        endpoint = "v1"
+    baseURL = "https://www.googleapis.com/customsearch/"
+    endpoint = "v1"
 
-        options = {
-                "key" : "AIzaSyCftZVPGOGCUitzwR9LyalIDSw2JFbJeGU",
-				"q" : query,
-				"cx" : "015219983740983411771%3Ad8srlihskhy",
-				"num" : "10"
-        }
+    options = {
+        "key" : "AIzaSyCftZVPGOGCUitzwR9LyalIDSw2JFbJeGU",
+        "q" : query,
+        "cx" : "015219983740983411771%3Ad8srlihskhy",
+        "num" : "10"
+    }
         
 
-        urlRequest = baseURL + endpoint + "?"
-        for key in options : 
-                urlRequest = urlRequest + key + "=" + options[key] + "&"
+    urlRequest = baseURL + endpoint + "?"
+    for key in options : 
+        urlRequest = urlRequest + key + "=" + options[key] + "&"
 
-        urlRequest = urlRequest[:-1]
+    urlRequest = urlRequest[:-1]
 
-        r = requests.get( urlRequest)
+    r = requests.get( urlRequest)
 
-        json_result = json.loads(r.text)
-		items = json_result["items"]
-			tab = []
+    json_result = json.loads(r.text)
+    items = json_result["items"]
+    tab = []
 		
-		for item in items : 
-			tab.append(item["link"])
+    for item in items : 
+        tab.append(item["link"])
 
-        return tab
+    return tab
 
 
 if __name__ == '__main__':
-        if len(sys.argv) != 2:
-                print('Usage: '+sys.argv[0]+' <JSON_File>')
-        else:
-                print get_URL_SSearch(sys.argv[1])
+    if len(sys.argv) != 2:
+        print('Usage: '+sys.argv[0]+' <JSON_File>')
+    else:
+        print get_URL_SSearch(sys.argv[1])
 
