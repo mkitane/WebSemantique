2145275bec5812805215afc4c5d5238ed68bbf95
Moreno Yassine <slifer.unit@gmail.com>
2014-11-18 10:39:47 +0100

    key+cx

--- SliferSearch2.js
+++ SliferSearch2.js
@@ -3,13 +3,13 @@ var request = require("superagent");
 request
    .get('https://www.googleapis.com/customsearch/v1')
    .query({
-		key : '',
-		cx : ' ',
+		key : 'AIzaSyCftZVPGOGCUitzwR9LyalIDSw2JFbJeGU',
+		cx : '015219983740983411771%3Ad8srlihskhy',
 		q : process.argv[2],
    })
    .end(function(res){
      if (res.ok) {
-       console.log;
+       console.log(res);
      } else {
        alert('Oh no! error ' + res.text);
      }
