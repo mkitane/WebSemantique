4055726cffdd405e9efe89af68a2f5f826cea0ba
Karim <karim2904@gmail.com>
2014-11-19 20:13:07 +0100

    Async spotlight requests

--- spotlight.js
+++ spotlight.js
@@ -12,32 +12,47 @@ function analyzeData(chunk)
 {
   var urlsAndText = JSON.parse(chunk);
 
+  // Array that will contain requests to dbpedia spotlight API for each search result
+  var requests = [];
+  
+  // Preparing requests functions
   urlsAndText.forEach(function(urlAndText, i)
-  {   
-        var uriObject = {
-            url: urlAndText.url
-        };
-    
-        request
-              .post('http://spotlight.dbpedia.org/rest/annotate')
-              .send('confidence=0.2')
-              .send('support=20')
-              .send('text='+encodeURI(urlAndText.text))
-              .set('Accept', 'application/json')
-              .end(function(res){
-                    if (res.ok) {
-                          var resourcesArray = [];
-                          res.body.Resources.forEach(function(resource, i) {
-                                resourcesArray.push(resource['@URI']);
-                          });
-                          uriObject.resources = resourcesArray;
-                          outputArray.push(uriObject);
-                    }
-                    
-                    if (i == urlsAndText.length-1) {
-                          console.log(outputArray);
-                    }
-              });      
+  {
+    var requestFunction = (function(urlAndText) {
+        return function(callback)
+        {
+            var uriObject = {
+                url: urlAndText.url
+            };
+  
+            request
+                  .post('http://spotlight.dbpedia.org/rest/annotate')
+                  .send('confidence=0.2')
+                  .send('support=20')
+                  .send('text='+encodeURI(urlAndText.text))
+                  .set('Accept', 'application/json')
+                  .end(function(res){
+                        if (res.ok) {
+                              var resourcesArray = [];
+                              res.body.Resources.forEach(function(resource, i) {
+                                    resourcesArray.push(resource['@URI']);
+                              });
+                              uriObject.resources = resourcesArray;
+                              callback(null, uriObject);
+                        }
+                        else {
+                              callback(true, null);
+                        }
+                  });
+        }
+    })(urlAndText);
+  
+    requests.push(requestFunction);
   });
-
+  
+  // Running requests in parallel
+  async.parallel(requests, function(err, uris)
+  {
+      console.log(uris);
+  });
 }
