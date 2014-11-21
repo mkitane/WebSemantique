bdb5526504231a80e8f49ab9e9756b9ff0cda7ed
Karim <karim2904@gmail.com>
2014-11-21 08:30:03 +0100

    Bug fix

--- spotlight.js
+++ spotlight.js
@@ -14,7 +14,7 @@ function analyzeData(chunk)
 
   // Array that will contain requests to dbpedia spotlight API for each search result
   var requests = [];
-  
+
   // Preparing requests functions
   urlsAndText.forEach(function(urlAndText, i)
   {
@@ -22,28 +22,34 @@ function analyzeData(chunk)
         return function(callback)
         {
             var uriObject = {
-                url: urlAndText.url
+                url: urlAndText.url,
+                uriObject: []
             };
-  
-            request
-                  .post('http://spotlight.dbpedia.org/rest/annotate')
-                  .send('confidence=0.2')
-                  .send('support=20')
-                  .send('text='+encodeURI(urlAndText.text))
-                  .set('Accept', 'application/json')
-                  .end(function(res){
-                        if (res.ok) {
-                              var resourcesArray = [];
-                              res.body.Resources.forEach(function(resource, i) {
-                                    resourcesArray.push(resource['@URI']);
-                              });
-                              uriObject.resources = resourcesArray;
-                              callback(null, uriObject);
-                        }
-                        else {
-                              callback(true, null);
-                        }
-                  });
+            if (urlAndText.text != "") {
+                request
+                .post('http://spotlight.dbpedia.org/rest/annotate')
+                .send('confidence=0.2')
+                .send('support=20')
+                .send('text='+encodeURI(urlAndText.text))
+                .set('Accept', 'application/json')
+                .end(function(res){
+                      if (res.ok) {
+                            var resourcesArray = [];
+                            res.body.Resources.forEach(function(resource, i) {
+                                  resourcesArray.push(resource['@URI']);
+                            });
+                            uriObject.resources = resourcesArray;
+                            callback(null, uriObject);
+                      }
+                      else {
+                            callback(true, null);
+                      }
+                });
+
+            }
+            else {
+                callback(null, uriObject);
+            }
         }
     })(urlAndText);
   
