aa064dd4feaea622448cd2479f60e97554391435
Moreno Yassine <slifer.unit@gmail.com>
2014-11-21 08:27:07 +0100

    SSPARQL

--- SliferSPARQL.js
+++ SliferSPARQL.js
@@ -5,8 +5,10 @@ process.stdin.on('readable',function() {
 	var buffer = process.stdin.read();
 	var URLIs = JSON.parse(buffer);
 	var query = 'SELECT * WHERE { ?s ?p ?o. FILTER(?s in (';
-	buffer.body.forEach(function(uri){
+	buffer.forEach(function(tab){
+		tab.ressources.forEach(function(uri){
 			query = query + '<' + uri + '>,';
+		}
 	});
 	query = query.slice(0,-1);
 	query = query + ')) }';
@@ -20,9 +22,7 @@ process.stdin.on('readable',function() {
 		}),
 		.end(
 			function(res){
-					
+					console.log(res.body);
 						}
 		);
-	});
-
 	});
