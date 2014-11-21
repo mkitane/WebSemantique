4cee015fac3e526803e00d77648d7b54ba045acb
mkitane <mehdikitane@gmail.com>
2014-11-21 08:20:46 +0100

    SparQL

--- SliferSPARQL.js
+++ SliferSPARQL.js
@@ -4,23 +4,25 @@ process.stdin.on('readable',function() {
 	
 	var buffer = process.stdin.read();
 	var URLIs = JSON.parse(buffer);
-	
-	
-	var query_sparql = process.argv[2];
-
+	var query = 'SELECT * WHERE { ?s ?p ?o. FILTER(?s in (';
+	buffer.body.forEach(function(uri){
+			query = query + '<' + uri + '>,';
+	});
+	query = query.slice(0,-1);
+	query = query + ')) }';
 	request
 		.get('http://dbpedia.org/sparql'),
 		.query({
-			query : '',
+			query : query,
 			default-graph-uri : 'http://dbpedia.org',
 			format : 'JSON',
 			timeout : '30000',
 		}),
 		.end(
 			function(res){
-						
+					
 						}
-		)
+		);
+	});
 
 	});
-});
