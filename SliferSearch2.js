d920feb9fddeadfeb289a030fceea432f46bb1e7
Karim <karim2904@gmail.com>
2014-11-28 00:28:43 +0100

    SliferSearch google

--- SliferSearch2.js
+++ SliferSearch2.js
@@ -6,13 +6,16 @@ request
 		key : 'AIzaSyCftZVPGOGCUitzwR9LyalIDSw2JFbJeGU',
 		cx : '015219983740983411771:d8srlihskhy',
 		q : process.argv[2],
-		num : "5"
+		num : "10"
    })
    .end(function(res){
      if (res.ok) {
         tab = [];
 		    res.body.items.forEach(function(item,i){
-		     tab.push(item.link);
+		     tab.push({
+			     url: item.link,
+			     desc: item.snippet
+		     });
 		    })
         console.log(tab);
      }
