da80d6525e964d6a295959f05297bb4d421a5e05
Karim <karim2904@gmail.com>
2014-11-28 10:27:29 +0100

    modifs

--- generateAllOutputs.sh
+++ generateAllOutputs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./SliferSearch.py obama > outputs/SliferSearch.json
+./SliferSearch.py $1 > outputs/SliferSearch.json
 cat outputs/SliferSearch.json | ./textURL.py > outputs/textURL.json
 cat outputs/textURL.json | node spotlight.js > outputs/spotlight.json
 cat outputs/spotlight.json | ./SliferSPARQL.py > outputs/SliferSPARQL.json
